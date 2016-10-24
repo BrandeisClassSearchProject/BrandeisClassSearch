b46960d4f368148973e7c431bd01cf23f4bf3dc1
rozoalex <alexhubnds@icloud.com>
2016-10-23 23:44:43 -0400

    update structures

--- app/src/main/java/brandeisclasssearchproject/cs/brandies/edu/brandeisclasssearch/MainActivity.java
+++ app/src/main/java/brandeisclasssearchproject/cs/brandies/edu/brandeisclasssearch/MainActivity.java
@@ -3,6 +3,7 @@ package brandeisclasssearchproject.cs.brandies.edu.brandeisclasssearch;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,27 +13,42 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.Toast;
 
+import java.util.List;
+
+/*
+Tutorials about phrasing XML:
+https://developer.android.com/training/basics/network-ops/xml.html
+ */
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //default generated code.
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        //declare the fab buttom, which is the round button floating on the screen
+
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });//set the on Click of fab buttom
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        //Find the drawer
+        //which is the entire layout we can see
+
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
@@ -40,18 +56,87 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        //default generated code.
+
+
+        //The list view is the one we put all the infomations
+        ListView lv = (ListView) findViewById(R.id.theContentList);
+        SearchView sv = (SearchView) findViewById(R.id.searchClass);
+
+
+        sv.setOnQueryTextListener(new SearchView.OnQueryTextListener(){
+
+
+            /**
+             * Called when the user submits the query. This could be due to a key press on the
+             * keyboard or due to pressing a submit button.
+             * The listener can override the standard behavior by returning true
+             * to indicate that it has handled the submit request. Otherwise return false to
+             * let the SearchView handle the submission by launching any associated intent.
+             *
+             * @param query the query text that is to be submitted
+             * @return true if the query has been handled by the listener, false to let the
+             * SearchView perform the default action.
+             */
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                List<String> userInput=inpInterpreter.phraseRowInput(query);
+                //String targetURL
+                return false;
+            }
+
+            /**
+             * Called when the query text is changed by the user.
+             *
+             * @param newText the new content of the query text field.
+             * @return false if the SearchView should perform the default action of showing any
+             * suggestions if available, true if the action was handled by the listener.
+             */
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+
+
+        });
+
+
+
+
     }
 
+
+
+
+
+    /*
+    The method defines the behaviour of user clicking the back button
+    The back button is the triangle button on the bottom of the screen
+    it can be on the right or left depending on the systems
+    usually on the left
+     */
     @Override
     public void onBackPressed() {
+        Log.i("mylog","onBackPressed");
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
+            //Toast.makeText(getApplicationContext(), "close", Toast.LENGTH_SHORT).show();
             drawer.closeDrawer(GravityCompat.START);
         } else {
             super.onBackPressed();
         }
     }
 
+
+
+
+
+
+    /*
+    Option Menu stuffs
+    set up and inflate option menu
+    and defines select behaviors
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -73,7 +158,20 @@ public class MainActivity extends AppCompatActivity
 
         return super.onOptionsItemSelected(item);
     }
+    /*
+    Option Menu stuffs
+     */
 
+
+
+
+
+    /*
+    This method defines the behavior of click items in the navigation bar
+    Important
+    implement it later
+    after implementation of the searching mechanisms
+     */
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
---
+++ app/src/main/java/brandeisclasssearchproject/cs/brandies/edu/brandeisclasssearch/inpInterpreter.java
@@ -0,0 +1,378 @@
+package brandeisclasssearchproject.cs.brandies.edu.brandeisclasssearch;
+
+import java.util.List;
+
+/*the only purpose of this class is to convert the user's input
+  have not implemented yet
+  */
+public class inpInterpreter {
+
+
+    //use this method to phrase the row input
+    public static List<String> phraseRowInput(String input){
+
+        return null;
+    }
+
+    /*The following xml is copied from the class search
+    The phraseRowInput have to be able to take in
+    for example "cs 131"
+    and return a list of Strings ,Computer Science, 1400, 131
+    a constant time search is preferable
+    maybe implemented with a hashMap
+    or take advantages of android XmlPullParser and have done the searched on web
+    In this way, it's also guaranteed to be undated
+    /*
+    <option value="100" selected>
+              African and Afro-American Studies
+            </option>
+
+            <option value="200" >
+              American Studies
+            </option>
+
+            <option value="300" >
+              Anthropology
+            </option>
+
+            <option value="400" >
+              Arabic Language and Literature
+            </option>
+
+            <option value="425" >
+              Architectural Studies
+            </option>
+
+            <option value="450" >
+              Art History
+            </option>
+
+            <option value="500" >
+              Biochemistry
+            </option>
+
+            <option value="510" >
+              Biochemistry and Biophysics
+            </option>
+
+            <option value="600" >
+              Biological Physics
+            </option>
+
+            <option value="700" >
+              Biology
+            </option>
+
+            <option value="850" >
+              Biotechnology
+            </option>
+
+            <option value="900" >
+              Business
+            </option>
+
+            <option value="1000" >
+              Chemistry
+            </option>
+
+            <option value="1100" >
+              Chinese
+            </option>
+
+            <option value="1200" >
+              Classical Studies
+            </option>
+
+            <option value="1250" >
+              Comparative Humanities
+            </option>
+
+            <option value="1300" >
+              Comparative Literature and Culture
+            </option>
+
+            <option value="1400" >
+              Computer Science
+            </option>
+
+            <option value="1425" >
+              Creative Writing
+            </option>
+
+            <option value="1475" >
+              Creativity, the Arts, and Social Transformation
+            </option>
+
+            <option value="1500" >
+              East Asian Studies
+            </option>
+
+            <option value="1600" >
+              Economics
+            </option>
+
+            <option value="1700" >
+              Education
+            </option>
+
+            <option value="1800" >
+              English
+            </option>
+
+            <option value="1850" >
+              English as a Second Language
+            </option>
+
+            <option value="1900" >
+              Environmental Studies
+            </option>
+
+            <option value="2000" >
+              European Cultural Studies
+            </option>
+
+            <option value="2050" >
+              Experiential Learning
+            </option>
+
+            <option value="2100" >
+              Film, Television and Interactive Media
+            </option>
+
+            <option value="2300" >
+              Fine Arts
+            </option>
+
+            <option value="8000" >
+              First Year Seminars (FYS)
+            </option>
+
+            <option value="2400" >
+              French and Francophone Studies
+            </option>
+
+            <option value="2450" >
+              Genetic Counseling
+            </option>
+
+            <option value="2500" >
+              German Studies
+            </option>
+
+            <option value="2525" >
+              German, Russian, and Asian Languages and Literature
+            </option>
+
+            <option value="2535" >
+              Global Studies
+            </option>
+
+            <option value="2550" >
+              Greek
+            </option>
+
+            <option value="2700" >
+              Health: Science, Society, and Policy
+            </option>
+
+            <option value="2800" >
+              Hebrew
+            </option>
+
+            <option value="2900" >
+              Heller School for Social Policy and Management
+            </option>
+
+            <option value="2950" >
+              Hindi
+            </option>
+
+            <option value="6600" >
+              Hispanic Studies
+            </option>
+
+            <option value="3000" >
+              History
+            </option>
+
+            <option value="3100" >
+              History of Ideas
+            </option>
+
+            <option value="3200" >
+              Hornstein Jewish Professional Leadership Program
+            </option>
+
+            <option value="3300" >
+              Independent Interdisciplinary Major
+            </option>
+
+            <option value="3400" >
+              International and Global Studies
+            </option>
+
+            <option value="3600" >
+              International Business School
+            </option>
+
+            <option value="3700" >
+              Internship
+            </option>
+
+            <option value="3900" >
+              Islamic and Middle Eastern Studies
+            </option>
+
+            <option value="4000" >
+              Italian Studies
+            </option>
+
+            <option value="4100" >
+              Japanese
+            </option>
+
+            <option value="4200" >
+              Journalism
+            </option>
+
+            <option value="4225" >
+              Justice Brandeis Semester
+            </option>
+
+            <option value="4235" >
+              Korean
+            </option>
+
+            <option value="4600" >
+              Language and Linguistics
+            </option>
+
+            <option value="4250" >
+              Latin
+            </option>
+
+            <option value="4300" >
+              Latin American and Latino Studies
+            </option>
+
+            <option value="4400" >
+              Legal Studies
+            </option>
+
+            <option value="4700" >
+              Mathematics
+            </option>
+
+            <option value="4800" >
+              Medieval and Renaissance Studies
+            </option>
+
+            <option value="4900" >
+              Molecular and Cell Biology
+            </option>
+
+            <option value="5000" >
+              Music
+            </option>
+
+            <option value="5100" >
+              Near Eastern and Judaic Studies
+            </option>
+
+            <option value="5200" >
+              Neuroscience
+            </option>
+
+            <option value="5300" >
+              Peace, Conflict, and Coexistence Studies
+            </option>
+
+            <option value="5400" >
+              Philosophy
+            </option>
+
+            <option value="5500" >
+              Physical Education
+            </option>
+
+            <option value="5600" >
+              Physics
+            </option>
+
+            <option value="5700" >
+              Politics
+            </option>
+
+            <option value="5750" >
+              Portuguese
+            </option>
+
+            <option value="5800" >
+              Postbaccalaureate Premedical Studies
+            </option>
+
+            <option value="5900" >
+              Psychology
+            </option>
+
+            <option value="5950" >
+              Quantitative Biology
+            </option>
+
+            <option value="6000" >
+              Religious Studies
+            </option>
+
+            <option value="6100" >
+              Romance Studies
+            </option>
+
+            <option value="6300" >
+              Russian Studies
+            </option>
+
+            <option value="6325" >
+              Sculpture
+            </option>
+
+            <option value="6350" >
+              Sexuality and Queer Studies
+            </option>
+
+            <option value="6400" >
+              Social Justice and Social Policy
+            </option>
+
+            <option value="6500" >
+              Sociology
+            </option>
+
+            <option value="6550" >
+              South Asian Studies
+            </option>
+
+            <option value="6625" >
+              Spanish Language and Literature
+            </option>
+
+            <option value="6675" >
+              Studio Art
+            </option>
+
+            <option value="6700" >
+              Theater Arts
+            </option>
+
+            <option value="7050" >
+              University Writing (COMP and UWS)
+            </option>
+
+            <option value="6900" >
+              Women's, Gender, and Sexuality Studies
+            </option>
+
+            <option value="7000" >
+              Yiddish
+            </option>
+     */
+}
