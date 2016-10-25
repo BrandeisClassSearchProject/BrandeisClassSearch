1debc28441aedf3b45640f639460397e129d8a7c
hangyanjiang <jiangh@brandeis.edu>
2016-10-24 20:38:12 -0400

    professor class

---
+++ app/src/main/java/brandeisclasssearchproject/cs/brandies/edu/brandeisclasssearch/Professor.java
@@ -0,0 +1,14 @@
+package brandeisclasssearchproject.cs.brandies.edu.brandeisclasssearch;
+
+/**
+ * Created by hangyanjiang on 2016/10/24.
+ */
+
+public class Professor {
+    private String name;
+
+
+    public Professor(String name) {
+        this.name = name;
+    }
+}
