51b34fb5822265e27ce7a342c72e776f71ef2ebb
hangyanjiang <jiangh@brandeis.edu>
2016-11-24 18:58:35 -0500

    a

--- app/src/main/java/brandeisclasssearchproject/cs/brandies/edu/brandeisclasssearch/activities/ScheduleTable.java
+++ app/src/main/java/brandeisclasssearchproject/cs/brandies/edu/brandeisclasssearch/activities/ScheduleTable.java
@@ -1,6 +1,7 @@
 package brandeisclasssearchproject.cs.brandies.edu.brandeisclasssearch.activities;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,24 +13,22 @@ import brandeisclasssearchproject.cs.brandies.edu.brandeisclasssearch.database.D
 
 /**
  * Larry's proposal:
- *      this class receives a string as database name
- *      and, reading from that database, creates a schedule
- *      table.
+ *      this class receives a string as database name and table name
+ *      then, reading from that database, creates a schedule table.
  */
 
 public class ScheduleTable extends AppCompatActivity {
 
     SQLiteDatabase db;
     DBOpenHelper dbOpenHelper;
+    Cursor cursor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule_table);
+
+        Intent i =getIntent();
+
     }
-
-    Intent i =getIntent();
-    String databaseName =i.getExtras().getString("databaseName");
-
-
 }
--- app/src/main/java/brandeisclasssearchproject/cs/brandies/edu/brandeisclasssearch/database/DBOpenHelper.java
+++ app/src/main/java/brandeisclasssearchproject/cs/brandies/edu/brandeisclasssearch/database/DBOpenHelper.java
@@ -9,44 +9,76 @@ import android.database.sqlite.SQLiteOpenHelper;
 /**
  * Created by Larry on 2016/11/24.
  *
- * stealed from my hw3, haven't modified.
+ * stolen from my hw3.
+ *
+ * database table should have columns:  course_name (string)
+ *                                      course_time (string)
+ *
+ * Still need clear entire table method.
  */
 
 public class DBOpenHelper extends SQLiteOpenHelper {
 
-    public static final String DATABASE_NAME = "myDatabase.db1";
-    public static final String DATABASE_TABLE = "entry1";
-    public static final String description = "description";
-    public static final String note = "note";
-    public static final String date = "date";
-    public static final String id = "_id";
+    public static final String DATABASE_NAME = "coursesForSchedule.db";
+
+    // table names
+    public static final String TABLE_COURSE_SELECTION = "tableForCourseSelection";
+    public static final String TABLE_COURSE_TRACK = "tableForCourseTrack";
+
+    // column names
+    public static final String KEY_COURSE_NAME = "courseName";
+    public static final String KEY_COURSE_TIME = "courseTime";
+    public static final String KEY_ID = "_id";
     public static final int DATABASE_VERSION = 1;
 
-    private static final String DATABASE_CREATE = "CREATE TABLE "
-            + DATABASE_TABLE
-            + " (" + id
+    private static final String DATABASE_COURSE_SELECTION_CREATE = "CREATE TABLE "
+            + TABLE_COURSE_SELECTION
+            + " (" + KEY_ID
             + " INTEGER PRIMARY KEY AUTOINCREMENT,"
-            + description + " TEXT NOT NULL,"
-            + note + " TEXT,"
-            + date + " TEXT);";
+            + KEY_COURSE_NAME + " TEXT NOT NULL,"
+            + KEY_COURSE_TIME + " TEXT);";
+
+    private static final String DATABASE_COURSE_TRACK_CREATE = "CREATE TABLE "
+            + TABLE_COURSE_TRACK
+            + " (" + KEY_ID
+            + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+            + KEY_COURSE_NAME + " TEXT NOT NULL,"
+            + KEY_COURSE_TIME + " TEXT);";
 
     public DBOpenHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
-    public void addLog(String des, String note, String date, SQLiteDatabase db) {
-        ContentValues contentValues = new ContentValues();
-        contentValues.put(this.description,des);
-        contentValues.put(this.note,note);
-        contentValues.put(this.date,date);
-        db.insert(DATABASE_TABLE,null,contentValues);
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        // creating required tables
+        db.execSQL(TABLE_COURSE_SELECTION);
+        db.execSQL(TABLE_COURSE_TRACK);
     }
 
-    public Cursor getLog(SQLiteDatabase db) {
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // on upgrade drop older tables
+        db.execSQL("DROP TABLE IF EXISTS" + TABLE_COURSE_SELECTION);
+        db.execSQL("DROP TABLE IF EXISTS" + TABLE_COURSE_TRACK);
+
+        // create new tables
+        onCreate(db);
+    }
+
+    public void addLog(String courseName, String courseTime, String table, SQLiteDatabase db) {
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(this.KEY_COURSE_NAME,courseName);
+        contentValues.put(this.KEY_COURSE_TIME,courseTime);
+        db.insert(table,null,contentValues);
+    }
+
+    public Cursor getLog(String table, SQLiteDatabase db) {
         Cursor cursor;
 
-        String[] projections = {this.id, this.description, this.note, this.date};
-        cursor = db.query(this.DATABASE_TABLE, projections, null, null, null, null, null);
+        String[] projections = {this.KEY_ID, this.KEY_COURSE_NAME, this.KEY_COURSE_TIME,};
+        cursor = db.query(table, projections, null, null, null, null, null);
         if (cursor != null) {
 
             cursor.moveToFirst();
@@ -55,19 +87,8 @@ public class DBOpenHelper extends SQLiteOpenHelper {
         return cursor;
     }
 
-    public void deleteLog(long id, SQLiteDatabase db) {
-        db.delete(this.DATABASE_TABLE, this.id + "=" + id, null);
-    }
-
-    @Override
-    public void onCreate(SQLiteDatabase db) {
-        db.execSQL(DATABASE_CREATE);
-    }
-
-    @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS" + DATABASE_TABLE);
-        onCreate(db);
+    public void deleteLog(long id, String table, SQLiteDatabase db) {
+        db.delete(table, this.KEY_ID + "=" + id, null);
     }
 }
 
