a7899cb86543a64cdb88274212dcdf4f6c33818c
rozoalex <alexhubnds@icloud.com>
2016-11-26 10:27:43 -0500

    changes

--- app/src/main/java/brandeisclasssearchproject/cs/brandies/edu/brandeisclasssearch/fragments/FragmentMyClasses.java
+++ app/src/main/java/brandeisclasssearchproject/cs/brandies/edu/brandeisclasssearch/fragments/FragmentMyClasses.java
@@ -6,6 +6,10 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import java.util.List;
 
 import brandeisclasssearchproject.cs.brandies.edu.brandeisclasssearch.R;
 
@@ -23,8 +27,29 @@ public class FragmentMyClasses extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_classes, container, false);
+        View v = inflater.inflate(R.layout.fragment_my_classes, container, false);
+        ListView lv= (ListView)getView().findViewById(R.id.classList);
+
+        lv.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                //delete the current entry
+                //by get the id of this item
+                //delete in database
+                return false;
+            }
+        });
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+        });
+
+
+
+        return v;
     }
 
 }
